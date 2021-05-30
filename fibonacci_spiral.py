6c8a23cb59c3c23ef23537871865a8edc02c68c3
Miguel Coimbra <mcoimbra@users.noreply.github.com>
2021-05-31 00:55:56 +0100

    Testing the code.

--- fibonacci_spiral.py
+++ fibonacci_spiral.py
@@ -1,9 +1,15 @@
 import argparse
+
+import pprint
+
+from typing import Dict, List, Tuple, Set
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.patches import Rectangle, Arc
 from matplotlib.collections import PatchCollection
 import numpy as np
+
 
 
 def fib(n):
@@ -23,17 +29,19 @@ def fib(n):
     a, b = 1, 1
     for _ in range(n):
         yield a
-        a, b = b, a + b
+        #a, b = b, a + b
+        a, b = b, (a + b)
 
 
 def plot_fibonacci_spiral(
-    number_of_squares,
-    numbers=True,
-    arc=True,
-    cmap="Blues",
-    alpha=1,
-    golden_ratio=False,
-    filename="plot.png",
+    number_of_squares: int,
+    numbers: bool = True,
+    arc: bool = True,
+    line_width: int = 8,
+    cmap: str = "Blues",
+    alpha: float = 1,
+    golden_ratio: bool = False,
+    file_name: str = "plot.png"
 ):
     """Short summary.
 
@@ -45,12 +53,16 @@ def plot_fibonacci_spiral(
         Description of parameter `numbers`.
     arc : type
         Description of parameter `arc`.
+    line_width: type
+        Description of parameter `line_width`.
     cmap : type
         Description of parameter `cmap`.
     alpha : type
         Description of parameter `alpha`.
     golden_ratio : type
         Description of parameter `golden_ratio`.
+    file_name : type
+        Description of parameter `file_name`.
 
     Returns
     -------
@@ -58,18 +70,28 @@ def plot_fibonacci_spiral(
         Description of returned object.
 
     """
-    fibs = list(fib(number_of_squares))
-    x = 0
-    y = 0
-    angle = 180
+    fibs: List[int] = list(fib(number_of_squares))
+
+    pprint.pprint(fibs)
+
+    x: int = 0
+    y: int = 0
+    angle: int = 180
     center = (x + fibs[0], y + fibs[0])
-    rectangles = []
+    rectangles: List[Rectangle] = []
     xs, ys = [], []
 
-    fig, ax = plt.subplots(1, figsize=(16, 16))
+    #fig, ax = plt.subplots(1, figsize=(16, 16))
+    fig, ax = plt.subplots(1, figsize=(32, 32))
     for i, side in enumerate(fibs):
 
+        #side = int(side / 4)
+
+        pprint.pprint("> {}-th Rectangle center: {} width: {} height: {}".format(i, [x, y], side, side))
+
+
         rectangles.append(Rectangle([x, y], side, side))
+        # rectangles.append(Rectangle([x, y], side, side, linewidth=line_width))
         if numbers and i > number_of_squares - 8:
             ax.annotate(side, xy=(x + 0.45 * side, y + 0.45 * side), fontsize=14)
 
@@ -78,11 +100,11 @@ def plot_fibonacci_spiral(
                 center,
                 2 * side,
                 2 * side,
-                angle=angle,
-                theta1=0,
-                theta2=90,
-                edgecolor="black",
-                antialiased=True,
+                angle = angle,
+                theta1 = 0,
+                theta2 = 90,
+                edgecolor = "black",
+                antialiased = True,
             )
             ax.add_patch(this_arc)
         angle += 90
@@ -110,9 +132,15 @@ def plot_fibonacci_spiral(
             x -= previous_side
             y += side
             center = (x, y)
+
+        pprint.pprint("> center:\t {}".format(center))
         previous_side = side
 
-    col = PatchCollection(rectangles, alpha=alpha, edgecolor="black")
+    # col = PatchCollection(rectangles, alpha=alpha, edgecolor="black")
+
+    col: PatchCollection = PatchCollection(rectangles, alpha=alpha, edgecolor="black", linewidths=line_width)
+
+
     try:
         col.set(array=np.asarray(range(number_of_squares + 1)), cmap=cmap)
     except ValueError:
@@ -138,13 +166,13 @@ def plot_fibonacci_spiral(
     if golden_ratio:
         plt.title(r"$\varphi$ = " + gr)
     plt.tight_layout()
-    plt.savefig(filename)
+    plt.savefig(file_name)
 
 
 if __name__ == "__main__":
 
-    description = "Creates a plot for a fibonacci spiral"
-    parser = argparse.ArgumentParser(description=description)
+    description: str = "Creates a plot for a fibonacci spiral"
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description=description)
     parser.add_argument(
         "-n",
         "--number_of_squares",
@@ -200,13 +228,26 @@ if __name__ == "__main__":
         required=False,
     )
 
-    args = parser.parse_args()
+    parser.add_argument(
+        "-l",
+        "--line-width",
+        dest="line_width",
+        type=int,
+        help="matplotlib Rectangle line width",
+        default=10,
+        required=False,
+    )
+
+    args: argparse.Namespace = parser.parse_args()
+
+    target_file_name: str = "figures/fib-plot-n{}-l{}.png".format(args.number_of_squares, args.line_width)
 
     plot_fibonacci_spiral(
-        args.number_of_squares,
-        numbers=bool(args.labels),
-        arc=bool(args.arc),
-        cmap=args.cmap,
-        alpha=args.alpha,
-        filename=args.output,
+        number_of_squares = args.number_of_squares,
+        numbers = bool(args.labels),
+        arc = bool(args.arc),
+        line_width = args.line_width,
+        cmap = args.cmap,
+        alpha = args.alpha,
+        file_name = target_file_name,
     )
